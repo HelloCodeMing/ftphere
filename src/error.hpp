80d8c5f46d4faa7bdcb0922b6195d3533528425c
HelloCodeMing <huanmingwong@163.com>
2015-05-04 22:53:07 +0800

    use exception to handle errors

---
+++ src/error.hpp
@@ -0,0 +1,6 @@
+#define ERROR_HPP
+
+class BadCMD: public std::exception {
+    public:
+        BadCMD(const char* msg)
+}
--- src/ftp_server.hpp
+++ src/ftp_server.hpp
@@ -4,30 +4,42 @@
  * It could be used to transport files conveniently.
  */
 
+#define FTPSERVER_HPP
 
 #include <boost/asio.hpp>
 #include <iostream>
+#include <stdexcept>
+
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+#include "logger.hpp"
+#endif
 
 namespace ftp {
 
 using namespace boost::asio;
 using boost::asio::ip::tcp;
 using std::string;
+using std::exception;
+using boost::system::error_code;
 
 class FTPServer {
     public:
         FTPServer(): 
-            port_(8080),
+            port_(21),
             local_ep_(tcp::v4(), port_),
             ios_(),
-            acceptor_(ios_) {
+            acceptor_(ios_),
+            logger_("./ftpserver.log"){
             
         }
 
-        ~FTPServer() {}
+        ~FTPServer() {
+
+        }
 
         void Run() {
-            boost::system::error_code ec;
+            error_code ec;
             tcp::socket remote_socket(ios_);
 
             /* init the acceptor */
@@ -40,7 +52,7 @@ class FTPServer {
                 HandleClient(remote_socket);
                 if (ec) {
                     // to do
-                    Error(ec.what());
+                    Error(ec.message());
                 }
             }
         }
@@ -49,10 +61,24 @@ class FTPServer {
     private:
         enum CMD { USER, PASS, PORT };
 
+        std::pair<int, string>
+        ResolveCMD(const string& cmd) {
+            int delimit_index = cmd.find(' ');
+            if (delimit_index == string::npos) {
+                // to do
+                throw std::invalid_argument("bad cmd");
+            } else {
+            }
+        }
+
+        void Dispatch(tcp::socket& socket, int cmd, string args) {
+
+        }
+
         void HandleClient(tcp::socket& socket) {
             LogClientAction(socket, "sign in");
 
-            boost::system::error_code ec;
+            error_code ec;
             streambuf buff;
             std::istream is(&buff);
             string line;
@@ -63,7 +89,7 @@ class FTPServer {
                 
                 try {
                     auto cmd_args = ResolveCMD(line);
-                    Dispatch(tcp::socket& socket, cmd_args.first, cmd_args.second);
+                    Dispatch(socket, cmd_args.first, cmd_args.second);
                 } catch (exception& e) {
                     // to do
                     Error(e.what());
@@ -71,28 +97,24 @@ class FTPServer {
                 }
                 if (ec) {
                     // to do
-                    Error(ec.what());
+                    Error(ec.message());
                 }
             }
             socket.close();
         }
 
-        std::pair<int, string>
-        ResovleCMD(const string& cmd) {
-            int delimit_index = cmd.find(' ');
-            if (delimit_index == string::npos) {
-                // to do
-                throw 
-            }
-        }
 
         void LogClientAction(tcp::socket& socket, const char* msg) {
-            tcp::address address = socket.remote_endpoint().address();
+            ip::address address = socket.remote_endpoint().address();
             string info = address.to_string() + ": " + msg;
             logger_.Log(info, Logger::INFO);
         }
 
         void Error(const char* msg) {
+            logger_.Log(msg, Logger::ERROR);
+        }
+
+        void Error(const string& msg) {
             logger_.Log(msg, Logger::ERROR);
         }
 
