8f64529b74c7b8628e11054301279eb3cd5d2248
HelloCodeMing <huanmingwong@163.com>
2015-05-04 11:11:17 +0800

    finish logger

--- src/test.cc
+++ src/test.cc
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cstdio>
+#include <cassert>
 
 #include "logger.hpp"
 
 using namespace ftp;
 
-int main()
-{
+bool TestLogger() {
     const char* log_path = "./test.log";
     Logger logger(log_path);
     logger.Log("wow, amazing", Logger::INFO);
+    return true;
+}
+int main()
+{
+    assert(TestLogger());
     return 0;
 }
